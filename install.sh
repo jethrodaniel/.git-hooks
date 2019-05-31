abf2c8e6484d166b0848f778b1e05a3442f4e6e9
Mark Delk <jethrodaniel@gmail.com>
2019-05-30 22:13:08 -0500

    add install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if ! command -v gem
+then
+  echo '`gem` not found. Exiting...'
+  exit 1
+fi
+
+gem install overcommit \
+  && overcommit --install \
+  && git clone https://github.com/jethrodaniel/.git-hooks \
+  && cp .git-hooks/.overcommit.yml.example .overcommit.yml \
+  && overcommit --sign \
+  && overcommit --sign prepare-commit-msg \
+  && overcommit --sign commit-msg
